b96f56abf94339059bcb8715589e5502e2ac3669
ManiaC <imroot@maniacalipsis.ru>
2025-08-10 22:21:49 +0300

    Update:  - Improved extensibility of classes menu_customizations.php\{CleanMenuWalker,BareMenuWalker}.

--- menu_customizations.php
+++ menu_customizations.php
@@ -10,17 +10,38 @@
 
 namespace Maniacalipsis\Utilities;
 
-class CleanMenuWalker extends \Walker_Nav_Menu
+use \stdClass;
+use \Walker_Nav_Menu;
+use \WP_Post;
+use function \render_element_attrs;
+
+trait TMakeLinkAttrs
+{
+   protected function make_link_attrs(WP_Post $data_object,int $depth,null|array|stdClass $args):array
+   {
+      return [
+                "class"=>($data_object->classes ? implode(" ",$data_object->classes) : null),
+                "href"=>htmlspecialchars($data_object->url),
+                "target"=>$data_object->target,
+                "rel"=>$data_object->xfn,
+                "title"=>htmlspecialchars($data_object->attr_title),
+             ];
+   }
+}
+
+class CleanMenuWalker extends Walker_Nav_Menu
 {
    //Renders menu using <DIV> elements.
    //NOTE: This method does not respect filters.
    
+   use TMakeLinkAttrs;
+   
 	public function start_lvl(&$output,$depth=0,$args=[])
    {
-      $real_depth=$depth+1;
 		$indent=str_repeat("   ",$real_depth);
+      $lvl_attrs_str=render_element_attrs($this->make_lvl_attrs($depth,$args));
       
-      $output.="\n$indent<DIV CLASS=\"sub_menu l$real_depth\">\n";
+      $output.="\n$indent<DIV $lvl_attrs_str>\n";
 	}
 	
 	public function end_lvl(&$output,$depth=0,$args=array())
@@ -36,18 +57,10 @@ class CleanMenuWalker extends \Walker_Nav_Menu
       $real_depth=$depth+1;
 		$indent=str_repeat("   ",$real_depth);
       
-      $link_attrs=[
-                     "href"=>$data_object->url,
-                     "target"=>$data_object->target,
-                     "rel"=>$data_object->xfn,
-                     "title"=>$data_object->attr_title,
-                  ];
-      
-      //Additional className[s]:
-      foreach ($data_object->classes??[] as $class)
-         $link_attrs["class"].=" $class";
+      $elem_attrs_str=render_element_attrs($this->make_el_attrs($data_object,$depth,$args));
+      $link_attrs_str=render_element_attrs($this->make_link_attrs($data_object,$depth,$args));
 		
-      $output.="$indent<DIV CLASS=\"mi l$real_depth\">\n$indent<A".\render_element_attrs($link_attrs).">$data_object->title</A>\n";
+      $output.="$indent<DIV $elem_attrs_str>\n$indent<A $link_attrs_str>$data_object->title</A>\n";
 	}
 	
 	public function end_el(&$output,$data_object,$depth=0,$args=[])
@@ -57,11 +70,30 @@ class CleanMenuWalker extends \Walker_Nav_Menu
       
       $output.="$indent</DIV>\n";
    }
+   
+   protected function make_lvl_attrs(int $depth,null|array|stdClass $args):array
+   {
+      $real_depth=$depth+1;
+      
+      return [
+                "class"=>"sub_menu l$real_depth",
+             ];
+   }
+   
+   protected function make_el_attrs(WP_Post $data_object,int $depth,null|array|stdClass $args):array
+   {
+      $real_depth=$depth+1;
+      
+      return [
+                "class"=>"mi l$real_depth",
+             ];
+   }
 }
 
-class BareMenuWalker extends \Walker_Nav_Menu
+class BareMenuWalker extends Walker_Nav_Menu
 {
    //Renders bare menu links without submenus structure.
+   use TMakeLinkAttrs {TMakeLinkAttrs::make_link_attrs as _base_make_link_attrs;}
    
 	public function start_lvl(&$output,$depth=0,$args=[])
    {
@@ -78,26 +110,22 @@ class BareMenuWalker extends \Walker_Nav_Menu
       //Renders clean link for menu item.
       //NOTE: This method does not respect filters.
       
-      $real_depth=$depth+1;
-      
-      $link_attrs=[
-                     "class"=>"mi l$real_depth",
-                     "href"=>$data_object->url,
-                     "target"=>$data_object->target,
-                     "rel"=>$data_object->xfn,
-                     "title"=>$data_object->attr_title,
-                  ];
-      
-      //Additional className[s]:
-      foreach ($data_object->classes??[] as $class)
-         $link_attrs["class"].=" $class";
+      $link_attrs_str=render_element_attrs($this->make_link_attrs($data_object,$depth,$args));
 
-		$output.="<A".\render_element_attrs($link_attrs).">$data_object->title</A>";
+		$output.="<A $link_attrs_str>$data_object->title</A>";
 	}
 	
 	public function end_el(&$output,$data_object,$depth=0,$args=[])
    {
+      //Does nothing.
+   }
+   
+   protected function make_link_attrs(WP_Post $data_object,int $depth,null|array|stdClass $args):array
+   {
+      $res=$this->_base_make_link_attrs($data_object,$depth,$args);
+      $res["class"]="mi l$real_depth ".$res["class"];
       
+      return $res;
    }
 }
 
